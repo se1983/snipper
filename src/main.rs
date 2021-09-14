2d32aed31fb3c001a8e52c5872f83bf3a08fb145
sebastian <sebsch@geblubber.org>
2021-09-14 19:25:30 +0200

    Allow defining visibility in opts.

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,27 @@ use clap::Clap;
 use snipper::Mode::*;
 
 
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api = snipper::Api::new();
 
-    let resp = match api.config.mode {
-        Create => {
-            match api.get_snippet().await? {
+    let snippet = match api.config.mode {
+        Create => match api.get_snippet().await? {
                 Some(snippet) => snippet,
                 None => api.create_snippet().await?
             }
-        }
-        Update => {
-            match api.get_snippet().await?{
+
+        Update => match api.get_snippet().await?{
                 Some(snippet) => api.upload_file(snippet.id).await?,
                 None => panic!("Could not find snippet: {}", api.config.title)
-            }
+
         }
         Get => match api.get_snippet().await?{
             Some(snippet) => snippet,
             None => panic!("Could not find snippet: {}", api.config.title)
         }
     };
-
-    let response_text = serde_json::to_string_pretty(&resp).unwrap();
-    println!("{}", response_text);
+    println!("{}", serde_json::to_string_pretty(&snippet)?);
 
     Ok(())
 }
